3f8f6bb129fabcf83c38dba2066d840e3e046658
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-04 16:31:30 -0500

    Add: validations to property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,4 +2,9 @@ class Property < ApplicationRecord
   belongs_to :property_type
   belongs_to :address
   has_many :saved_properties
+
+  # validations
+  validates :active, :description, :photo_urls, presence :true
+  validates :bedrooms, :bathrooms, :area, numericality: { greater_than: 0}
+
 end
--- db/migrate/20221004153927_create_properties.rb
+++ db/migrate/20221004153927_create_properties.rb
@@ -6,7 +6,7 @@ class CreateProperties < ActiveRecord::Migration[7.0]
       t.integer :bedrooms
       t.integer :bathrooms
       t.integer :area
-      t.text :description
+      t.text :description, null: false
       t.text :photo_urls, array: true, default: []
       t.boolean :active, default: true
 

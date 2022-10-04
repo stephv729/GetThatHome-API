eeb00dac184cb0753b27250488c0403778e26076
Erik <efdree@gmail.com>
2022-10-04 16:48:49 -0500

    Add: Validation of property_for_rent, property_type, role, user

--- app/models/property_type.rb
+++ app/models/property_type.rb
@@ -1,3 +1,7 @@
 class PropertyType < ApplicationRecord
   has_many :properties
+
+    # Validations
+
+    validates :name, presence: true, uniqueness: false
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,7 @@
 class Role < ApplicationRecord
   has_many :users
+
+  # Validations
+
+  validates :name, presence: true, uniqueness: true 
 end

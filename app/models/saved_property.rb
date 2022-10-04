0ee502ca73169e57f3c48ab13fb3e4c14b2df6fa
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-04 16:39:40 -0500

    Add: validations to saved_property model

--- app/models/saved_property.rb
+++ app/models/saved_property.rb
@@ -1,4 +1,14 @@
 class SavedProperty < ApplicationRecord
   belongs_to :user
   belongs_to :property
+
+  after_update :clean
+
+  private
+
+  def clean
+    # Hace referencia que no tiene ni developer ni publisher y lo elimina si es el caso.
+    SavedProperty.delete(self) unless self.favorite|| self.contacted
+  end
+
 end
--- db/migrate/20221004154446_create_saved_properties.rb
+++ db/migrate/20221004154446_create_saved_properties.rb
@@ -3,8 +3,8 @@ class CreateSavedProperties < ActiveRecord::Migration[7.0]
     create_table :saved_properties do |t|
       t.references :user, null: false, foreign_key: true
       t.references :property, null: false, foreign_key: true
-      t.boolean :favorite
-      t.boolean :contacted
+      t.boolean :favorite, default: false
+      t.boolean :contacted, default: false
 
       t.timestamps
     end

373fbbd481603b34c6b4ef831a77e27d7fbc4eae
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-04 16:24:51 -0500

    Add: validations to property for sale model

--- app/models/property_for_sale.rb
+++ app/models/property_for_sale.rb
@@ -1,4 +1,7 @@
 class PropertyForSale < ApplicationRecord
   belongs_to :property
   has_one :own, as: :ownable, dependent: :destroy
+
+  # validations
+  validates :price, numericality: { greater_than: 0 }, presence :true
 end

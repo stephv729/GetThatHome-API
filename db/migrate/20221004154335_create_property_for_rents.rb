6b02db753aba078db6d03e631ceebff3307d453e
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-04 15:44:35 -0500

    Change: columns restrictions in prop for rent

--- db/migrate/20221004154335_create_property_for_rents.rb
+++ db/migrate/20221004154335_create_property_for_rents.rb
@@ -2,9 +2,9 @@ class CreatePropertyForRents < ActiveRecord::Migration[7.0]
   def change
     create_table :property_for_rents do |t|
       t.references :property, null: false, foreign_key: true
-      t.integer :monthly_rent
-      t.integer :maintenance
-      t.boolean :pets_allowed
+      t.integer :monthly_rent, null: false
+      t.integer :maintenance, null: false
+      t.boolean :pets_allowed, null: false
 
       t.timestamps
     end

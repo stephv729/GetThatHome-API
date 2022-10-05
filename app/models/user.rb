d54800daac062f72710b0c480b45cb682dfe75e5
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-05 16:40:26 -0500

    Change: validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
   # Validations
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, presence: true, uniqueness: false
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: VALID_EMAIL_REGEX }
   validates :phone, presence: true, uniqueness: true
 
-  validates :password, presence: true, uniqueness: false
+  validates :password, presence: true
 end

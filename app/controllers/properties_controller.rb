ddcbf1f2a04bcd1bbe3de415a7069046d4ab1a80
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-05 16:43:15 -0500

    Change:  properties_controller
    Co-authored-by: efdree <efdree@users.noreply.github.com>

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 class PropertiesController < ApplicationController
+  before_action :set_property, only: %i[show update destroy]
   def index
     @properties = Property.all # array
     render json: @properties
+  end
+
+  # GET /properties/1
+  def show
+    render json: @property, serializer: PropertyDetailsSerializer
+  end
+
+  def update; end
+
+  def destroy; end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_property
+    Rails.logger.debug @property = Property.find(params[:id])
   end
 end

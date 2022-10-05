f0e9526e87ee1ca99fdb919cf7e681160fc8824a
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-05 16:43:29 -0500

    Add: show property route

--- app/models/property.rb
+++ app/models/property.rb
@@ -10,12 +10,13 @@ class Property < ApplicationRecord
   validates :bedrooms, :bathrooms, :area, numericality: { greater_than_or_equal_to: 0 }
 
   def operation_type
-    type = "undefined"
-    if PropertyForSale.find_by(property_id: id).present?
-      type = "for sale"
-    elsif PropertyForRent.find_by(property_id: id).present?
-      type = "for rent"
+    for_sale = PropertyForSale.find_by(property_id: id)
+    for_rent = PropertyForRent.find_by(property_id: id)
+    if for_sale.present?
+      { type: "for sale", price: for_sale.price }
+    elsif for_rent.present?
+      { type: "for rent", monthly_rent: for_rent.monthly_rent, maintenance: for_rent.maintenance,
+        pets_allowed: for_rent.pets_allowed }
     end
-    type
   end
 end

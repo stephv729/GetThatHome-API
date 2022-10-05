8d4c637a396e619d23301e45101ac8c861970745
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-05 14:15:06 -0500

    Add: operation_type to property class

--- app/models/property.rb
+++ app/models/property.rb
@@ -8,4 +8,14 @@ class Property < ApplicationRecord
   # validations
   validates :area, :active, :description, presence: true
   validates :bedrooms, :bathrooms, :area, numericality: { greater_than_or_equal_to: 0 }
+
+  def operation_type
+    type = "undefined"
+    if PropertyForSale.find_by(property_id: id).present?
+      type = "for sale"
+    elsif PropertyForRent.find_by(property_id: id).present?
+      type = "for rent"
+    end
+    type
+  end
 end

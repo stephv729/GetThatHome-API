3941b1403326e74031bc613e00b2c238974cd607
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-06 06:02:37 -0500

    Add: rack-cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,10 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3001"
-
-    resource "*",
-             headers: :any,
-             methods: [:get]
+    origins '*'
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :options, :delete, :put, :patch]
   end
 end

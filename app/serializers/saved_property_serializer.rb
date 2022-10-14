2620441740cdac876609dd0c181fce01d82543f4
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-13 19:11:00 -0500

    Change: saved property serializer

--- app/serializers/saved_property_serializer.rb
+++ app/serializers/saved_property_serializer.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class SavedPropertySerializer < ActiveModel::Serializer
-  attributes :id, :favorite, :contacted
-  belongs_to :property
+  attributes :id, :favorite, :contacted, :property_details
+
+  def property_details
+    PropertySerializer.new(object.property, root: false)
+  end
 end

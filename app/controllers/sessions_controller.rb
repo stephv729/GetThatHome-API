b4f1e3fe535951d896423e04f4c84839136f9758
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-13 19:10:36 -0500

    Change: comments in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,5 @@
-# # frozen_string_literal: true
-
-# class SessionsController < ApplicationController
-#   def create
-#     user = User.find_by(email: params[:email])
-#     if user&.valid_password?(params[:password])
-#       # extract client from auth header
-#       client = request.headers["client"]
-
-#       # update token, generate updated auth headers for response
-#       new_auth_header = user.create_new_auth_token(client)
-#       token = new_auth_header['Authorization'].sub("Bearer ","")
-#       # update response with the header that will be required by the next request
-#       response.headers.merge!(new_auth_header)
-#       render json: user.as_json(only: %i[id email]).merge!({token: token}), status: :created
-#     else
-#       head(:unauthorized)
-#     end
-#   end
-
-#   def destroy; end
-# end
 class SessionsController < DeviseTokenAuth::SessionsController
   def render_create_success
-    # p @resource.authorization
     token = {
       "access-token": @token.token,
       "token-type": "Bearer",

bcb966ff36d33a483834ef0fbf1e7140140818ea
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-13 20:25:43 -0500

    Move: property_details method from own model to own serializer

--- app/models/own.rb
+++ app/models/own.rb
@@ -3,20 +3,4 @@
 class Own < ApplicationRecord
   belongs_to :user
   belongs_to :ownable, polymorphic: true
-
-  def property_details
-    Rails.application.eager_load!
-    model = ApplicationRecord.descendants.find {|model| model.name==ownable_type}
-    prop = model.find(ownable_id)
-    prop = Property.find(prop.property_id)
-    operation_type = prop.operation_type
-    prop_type = PropertyType.find(prop.property_type_id).as_json(except: %i[created_at updated_at id])
-    address = Address.find(prop.address_id).as_json(except: %i[created_at updated_at id])
-    prop.as_json(except: %i[created_at updated_at property_type_id address_id]).merge!({
-      operation_type: operation_type,
-      property_type: prop_type,
-      address: address
-      })
-  end
-  
 end
--- app/serializers/own_serializer.rb
+++ app/serializers/own_serializer.rb
@@ -2,4 +2,11 @@
 
 class OwnSerializer < ActiveModel::Serializer
   attributes :id, :ownable_type, :ownable_id, :property_details
+
+  def property_details
+    Rails.application.eager_load!
+    model = ApplicationRecord.descendants.find {|model| model.name==object.ownable_type}
+    prop_by_operation = model.find(object.ownable_id)
+    PropertySerializer.new(prop_by_operation.property, root: false)
+  end
 end

7ac8d38002d71d9052796df0bfac1956b0ce2d4a
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-19 16:06:58 -0500

    Change: seed file using faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,6 @@
-# frozen_string_literal: true
+require "faker"
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-puts 'start destroying data ...'
+Rails.logger.debug "start destroying data ..."
 Own.destroy_all
 SavedProperty.destroy_all
 PropertyForSale.destroy_all
@@ -16,81 +9,168 @@ Property.destroy_all
 User.destroy_all
 Role.destroy_all
 PropertyType.destroy_all
-puts 'end destroying data ...'
+Rails.logger.debug "end destroying data ..."
 
-puts 'start creating roles'
-role1 = Role.create(name: 'Homeseeker')
-role2 = Role.create(name: 'Landlord')
+# Roles
+Rails.logger.debug "start creating roles"
+roles = [
+  {
+    name: "Homeseeker"
+  }, {
+    name: "Landlord"
+  }
+]
 
-puts role1.persisted?
-puts role2.persisted?
-puts 'end creating roles'
+roles.each do |role_data|
+  role = Role.create(role_data)
+  unless role.persisted?
+    Rails.logger.debug role.errors.full_messages
+    Rails.logger.debug role
+  end
+end
+Rails.logger.debug "end creating roles"
 
-puts 'start creating users'
-user1 = User.create(name: 'Pepito', role: role1, email: 'test@mail.com', password: '123456', phone: '+51932580181')
-user2 = User.create(name: 'Juancito', role: role2, email: 'test1@mail.com', password: '123456', phone: '+555555')
+# Users
+Rails.logger.debug "start creating users"
+roles = Role.all.to_a
+5.times do
+  user = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.safe_email,
+    role: roles.sample,
+    password: "123456",
+    phone: Faker::PhoneNumber.unique.cell_phone_in_e164
+  )
+  unless user.persisted?
+    Rails.logger.debug user.errors.full_messages
+    Rails.logger.debug user
+  end
+end
+Rails.logger.debug "end creating users"
 
-puts user1.persisted?
-puts user2.persisted?
-puts 'end creating users'
+# Property Types
+Rails.logger.debug "start creating property types"
+types = [
+  {
+    name: "Apartment"
+  }, {
+    name: "House"
+  }
+]
 
-puts 'start creating property types'
-type1 = PropertyType.create(name: 'Apartment')
-type2 = PropertyType.create(name: 'House')
+types.each do |type_data|
+  type = PropertyType.create(type_data)
+  unless type.persisted?
+    Rails.logger.debug type.errors.full_messages
+    Rails.logger.debug type
+  end
+end
+Rails.logger.debug "end creating property types"
 
-puts type1.persisted?
-puts type2.persisted?
-puts 'end creating property types'
+# Addresses
+Rails.logger.debug "start creating addresses"
+20.times do
+  address = Address.create(
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+    name: Faker::Address.community
+  )
+  unless address.persisted?
+    Rails.logger.debug address.errors.full_messages
+    Rails.logger.debug address
+  end
+end
+Rails.logger.debug "end creating addresses"
 
-puts 'start creating addresses'
-address1 = Address.create(latitude: -12.73, longitude: -77.07, name: 'direccion 1')
-address2 = Address.create(latitude: -12.65, longitude: -77.001, name: 'direccion 2')
+# Properties
+Rails.logger.debug "start creating properties"
+urls = ["https://images.freeimages.com/images/large-previews/e85/house-1224030.jpg",
+        "https://media.istockphoto.com/photos/new-housing-estate-with-underground-garage-picture-id1287012612?s=612x612", "https://images.freeimages.com/images/large-previews/d5b/home-1224274.jpg", "https://images.freeimages.com/images/large-previews/4b4/beach-house-1225387.jpg"]
 
-puts address1.persisted?
-puts address2.persisted?
-puts 'end creating addresses'
+addresses = Address.all.to_a
+types = PropertyType.all.to_a
+20.times do
+  property = Property.create(
+    address: addresses.sample,
+    property_type: types.sample,
+    description: Faker::Lorem.paragraph_by_chars(number: 150, supplemental: false),
+    area: rand(500..2000),
+    photo_urls: urls.sample(rand(1...urls.length))
+  )
+  unless property.persisted?
+    Rails.logger.debug property.errors.full_messages
+    Rails.logger.debug property
+  end
+end
+Rails.logger.debug "end creating properties"
 
-puts 'start creating properties'
-url1 = 'https://images.freeimages.com/images/large-previews/e85/house-1224030.jpg'
-url2 = 'https://media.istockphoto.com/photos/new-housing-estate-with-underground-garage-picture-id1287012612?s=612x612'
-prop1 = Property.create(address: address1, property_type: type1, description: 'lovely place', area: '100',
-                        photo_urls: [url1])
-prop2 = Property.create(address: address1, property_type: type2, description: 'lovely place', area: '60',
-                        photo_urls: [url1, url2])
-prop3 = Property.create(address: address2, property_type: type2, description: 'lovely place', area: '80',
-                        photo_urls: [url1, url2])
+# Properties For Rent or Sale
+Rails.logger.debug "start creating properties for sale or rent"
+properties = Property.all.to_a
+rent_group_length = rand(1...properties.length)
+rent_group = properties.sample(rent_group_length)
+sale_group = properties - rent_group
 
-puts prop1.persisted?
-puts prop2.persisted?
-puts prop3.persisted?
-puts 'end creating properties'
+rent_group.each do |property|
+  rent = PropertyForRent.create(
+    property:,
+    monthly_rent: rand(8...30) * 100,
+    maintenance: rand(1...12) * 20,
+    pets_allowed: [true, false].sample
+  )
+  unless rent.persisted?
+    Rails.logger.debug rent.errors.full_messages
+    Rails.logger.debug rent
+  end
+end
 
-puts 'start creating properties for sale or rent'
-rent1 = PropertyForRent.create(property: prop1, monthly_rent: 100, maintenance: 10, pets_allowed: true)
-rent2 = PropertyForRent.create(property: prop2, monthly_rent: 200, maintenance: 20, pets_allowed: false)
-sale1 = PropertyForSale.create(property: prop3, price: 300)
+sale_group.each do |property|
+  sale = PropertyForSale.create(
+    property:,
+    price: rand(8...30) * 100
+  )
+  unless sale.persisted?
+    Rails.logger.debug sale.errors.full_messages
+    Rails.logger.debug sale
+  end
+end
+Rails.logger.debug "end creating properties for sale or rent"
 
-puts rent1.persisted?
-puts rent2.persisted? ? 'true' : rent2.errors.full_messages
-puts sale1.persisted?
-puts 'end creating properties for sale or rent'
+# Saved Properties
+Rails.logger.debug "start creating saved properties"
 
-puts 'start creating saved properties'
-favorite1 = SavedProperty.create(property: prop1, user: user1, favorite: true)
-favorite1copy = SavedProperty.create(property: prop1, user: user1, favorite: true)
-contacted1 = SavedProperty.create(property: prop2, user: user1, contacted: true)
+properties_to_save = properties.sample(rand(1...properties.length))
+homeseekers = User.where(role: Role.find_by(name: "Homeseeker"))
 
-puts favorite1.persisted?
-puts favorite1copy.persisted? ? 'true' : favorite1copy.errors.full_messages
-puts contacted1.persisted?
-puts 'end creating saved properties'
+properties_to_save.each do |property|
+  saved_property = SavedProperty.create(
+    property:,
+    user: homeseekers.sample,
+    favorite: [true, false].sample,
+    contacted: [true, false].sample
+  )
 
-puts 'start creating own'
-own1 = Own.create(user: user2, ownable: rent1)
-own2 = Own.create(user: user2, ownable: rent2)
-own3 = Own.create(user: user2, ownable: sale1)
+  unless saved_property.persisted?
+    Rails.logger.debug saved_property.errors.full_messages
+    Rails.logger.debug saved_property
+  end
+end
+Rails.logger.debug "end creating saved properties"
 
-puts own1.persisted?
-puts own2.persisted?
-puts own3.persisted?
-puts 'end creating own'
+# Ownable properties
+Rails.logger.debug "start creating own"
+landlords = User.where(role: Role.find_by(name: "Landlord")).to_a
+properties_to_own = PropertyForSale.all.to_a + PropertyForRent.all.to_a
+
+properties_to_own.each do |property|
+  ownable_property = Own.create(
+    ownable: property,
+    user: landlords.sample
+  )
+
+  unless ownable_property.persisted?
+    Rails.logger.debug ownable_property.errors.full_messages
+    Rails.logger.debug ownable_property
+  end
+end
+Rails.logger.debug "end creating own"

b8a0d3b79c9e04e3925081fe03691f9697f5a7eb
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-24 14:59:30 -0500

    Remove: before_action from main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,4 @@
 class MainController < ApplicationController
-  before_action :authenticate_user!, only: %i[my_properties show profile]
-
   def index
     render json: {msg: "This is the GetThatHome-API"}
   end

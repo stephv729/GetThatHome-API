ceae7e7d4ad422bd5d5f5f6756fac11498d2fe05
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-11-03 23:54:50 -0500

    Change: properties serializer to return updated_at

--- app/serializers/property_serializer.rb
+++ app/serializers/property_serializer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PropertySerializer < ActiveModel::Serializer
-  attributes :id, :bedrooms, :bathrooms, :area, :photo_urls, :active, :operation_type
+  attributes :id, :bedrooms, :bathrooms, :area, :photo_urls, :active, :operation_type, :updated_at
   belongs_to :property_type
   belongs_to :address
 end
